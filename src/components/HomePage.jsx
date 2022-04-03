e4e070dc0589b7d9066d5a3bb38761bb27105d8d
Sajid Hussain <sajidhussain98@gmail.com>
2022-04-03 22:34:34 +0530

    Global Crypto stats completed

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -6,8 +6,8 @@ import { useGetCryptosQuery } from '../services/CryptoApi';
 
 const HomePage = () => {
   const { data, isFetching } = useGetCryptosQuery();
-  console.log(data);
-
+  const globalStats=data?.data?.stats
+if (isFetching)  return 'Loading...'
   const { Title } = Typography;
   return (
     <>
@@ -16,20 +16,32 @@ const HomePage = () => {
       </Title>
       <Row>
         <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value="5" />
+          <Statistic title="Total Cryptocurrencies" value={globalStats.total} />
         </Col>
 
         <Col span={12}>
-          <Statistic title="Total Exchanges" value="5" />
+          <Statistic
+            title="Total Exchanges"
+            value={millify(globalStats.totalExchanges)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Market Cap:" value="5" />
+          <Statistic
+            title="Total Market Cap:"
+            value={millify(globalStats.totalMarketCap)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total 24h Volume" value="5" />
+          <Statistic
+            title="Total 24h Volume"
+            value={millify(globalStats.total24hVolume)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Markets" value="5" />
+          <Statistic
+            title="Total Markets"
+            value={millify(globalStats.totalMarkets)}
+          />
         </Col>
       </Row>
     </>
