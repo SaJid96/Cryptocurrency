4ba54ac6ecc645ead8162a85357fbeab8c433f2d
Sajid Hussain <sajidhussain98@gmail.com>
2022-04-05 01:08:28 +0530

    bing news search API integrated

---
+++ src/services/CryptoNewsApi.js
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+  'X-BingApis-SDK': 'true',
+  'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com',
+  'X-RapidAPI-Key': 'afb932572emsh6113b3e3a8c6416p14f5e6jsnb00775d99307',
+};
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com'
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl,
+  }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+        ),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
