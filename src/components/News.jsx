0e56d2fb2f2cfb02c7cc21c30917592e676ce84a
Sajid Hussain <sajidhussain98@gmail.com>
2022-04-06 00:25:44 +0530

    Updating state

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,8 +2,35 @@ import React, { useState } from 'react';
 import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
 import moment from 'moment';
 
-const News = () => {
-  return <div>News</div>;
+import {useGetCryptosQuery} from '../services/CryptoApi'
+import {useGetCryptoNewsQuery} from '../services/CryptoNewsApi'
+
+const { Text, Title } = Typography;
+const { Option } = Select;
+
+const News = ({simplified}) => {
+
+   const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
+   const {data}=useGetCryptosQuery(10);
+   const {data:cryptoNews}=useGetCryptoNewsQuery({newsCategory,count:simplified ? 6 :12})
+
+  return 
+  (
+  <Row gutter={[24,24]}>
+    {
+      !simplified && (
+
+        <Col span={24}>
+        
+        
+
+
+        </Col>
+
+      )
+    }
+    </Row>
+  )
 };
 
 export default News;
