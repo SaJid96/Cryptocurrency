fb790f5f51f2f3572fba8c090acdf7d1d7f33288
Sajid Hussain <sajidhussain98@gmail.com>
2022-04-06 01:42:58 +0530

    News

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -16,29 +16,67 @@ const News = ({simplified}) => {
 
    if(!cryptoNews?.value) return 'Loading...'
   return (
-  <Row gutter={[24, 24]}>
-    {!simplified && (
-      <Col span={24}>
-        <Select
-          showSearch
-          className="select-news"
-          placeholder="select a Crypto"
-          optionFilterProp="children"
-          onChange={(value) => setNewsCategory(value)}
-          filterOption={(input, option) =>
-            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-          }
-        >
-          <Option value="Cryptocurrency">Cryptocurrency</Option>
+    <Row gutter={[24, 24]}>
+      {!simplified && (
+        <Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            placeholder="select a Crypto"
+            optionFilterProp="children"
+            onChange={(value) => setNewsCategory(value)}
+            filterOption={(input, option) =>
+              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+            }
+          >
+            <Option value="Cryptocurrency">Cryptocurrency</Option>
 
-          {data?.data?.coins?.map((currency) => (
-            <Option value={currency.name}>{currency.name}</Option>
-          ))}
-        </Select>
-      </Col>
-    )}
-   
-  </Row>
-)}
+            {data?.data?.coins?.map((currency) => (
+              <Option value={currency.name}>{currency.name}</Option>
+            ))}
+          </Select>
+        </Col>
+      )}
+      {cryptoNews.value.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card hoverable className="news-card">
+            <a href={news.url} target="_blank" rel="noreferrer">
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>
+                  {news.name}
+                </Title>
+                <img
+                  src={news?.image?.thumbnail?.contentUrl 
+                  }
+                  alt=""
+                />
+              </div>
+              <p>
+                {news.description.length > 100
+                  ? `${news.description.substring(0, 100)}...`
+                  : news.description}
+              </p>
+              <div className="provider-container">
+                <div>
+                  <Avatar
+                    src={
+                      news.provider[0]?.image?.thumbnail?.contentUrl 
+                    }
+                    alt=""
+                  />
+                  <Text className="provider-name">
+                    {news.provider[0]?.name}
+                  </Text>
+                </div>
+                <Text>
+                  {moment(news.datePublished).startOf('ss').fromNow()}
+                </Text>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );}
 
 export default News;
