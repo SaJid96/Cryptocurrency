54688489e56dc6cb5ecca6f0b14f4c96d9b0db65
Sajid Hussain <sajidhussain98@gmail.com>
2022-04-06 23:48:10 +0530

    crypto price chart

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -37,6 +37,7 @@ const CryptoDetails = () => {
       });
      const cryptoDetails = data?.data?.coin;
 
+     if(isFetching) return 'loading...'
 
      const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
