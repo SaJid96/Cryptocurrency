3c13b15ca4c3af6dc2a2165c5d58f0df06dbf4cc
Sajid Hussain <sajidhussain98@gmail.com>
2022-04-06 22:20:10 +0530

    News Updated

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -11,7 +11,7 @@ const { Option } = Select;
 const News = ({simplified}) => {
 
    const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
-   const {data}=useGetCryptosQuery(10);
+   const {data}=useGetCryptosQuery(100);
    const {data:cryptoNews}=useGetCryptoNewsQuery({newsCategory,count:simplified ? 6 :12})
 
    if(!cryptoNews?.value) return 'Loading...'
