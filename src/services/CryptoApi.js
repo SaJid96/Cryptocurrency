bba04b1b7b6f0203617747f85af184fa4729a076
M Sajid Hussain <53912407+SaJid96@users.noreply.github.com>
2022-04-07 06:16:41 +0530

    Update CryptoApi.js

--- src/services/CryptoApi.js
+++ src/services/CryptoApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
   'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
-  'X-RapidAPI-Key': 'afb932572emsh6113b3e3a8c6416p14f5e6jsnb00775d99307',
+
 };
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
